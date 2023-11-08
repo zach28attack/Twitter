bcaab18ecf8560cafd67850481058614a674bbd1
Zachary Casares <rrumble250@gmail.com>
2023-11-08 14:23:45 -0500

    install react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
-import {useState} from "react";
 import "./App.css";
+import Home from "./pages/Home";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: "",
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        errorElement: "",
+      },
+    ],
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
