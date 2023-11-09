1089360fa18e8d6da54b5ab8809d14029fc8494e
Zachary Casares <rrumble250@gmail.com>
2023-11-09 14:49:50 -0500

    render feed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,10 @@
+import Feed from "../components/feed/Feed";
 import Class from "./Home.module.css";
 
 export default function Home() {
-  return <div className={Class.container}></div>;
+  return (
+    <div className={Class.container}>
+      <Feed />
+    </div>
+  );
 }
