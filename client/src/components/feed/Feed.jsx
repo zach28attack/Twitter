be5c2d5171e5cb3420c4cb75f30d8c5bbc5aa4f3
Zachary Casares <rrumble250@gmail.com>
2023-11-09 14:49:07 -0500

    create feed component

---
+++ client/src/components/feed/Feed.jsx
@@ -0,0 +1,10 @@
+import Class from "./Feed.module.css";
+import Tab from "./Tab";
+
+export default function Feed() {
+  return (
+    <div className={Class.container}>
+      <Tab />
+    </div>
+  );
+}
