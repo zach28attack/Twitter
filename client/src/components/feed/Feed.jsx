9732fcd0b476928ae0e48d2b834007c270c59032
Zachary Casares <rrumble250@gmail.com>
2023-11-10 15:23:41 -0500

    render tweet composition

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,10 +1,12 @@
 import Class from "./Feed.module.css";
 import Tab from "./Tab";
+import TweetComposition from "./TweetComposition";
 
 export default function Feed() {
   return (
     <div className={Class.container}>
       <Tab />
+      <TweetComposition />
     </div>
   );
 }
