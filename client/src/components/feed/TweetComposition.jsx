5ca7984d2b2b457bd9b9af8a47ee50f0d5e90d0d
Zachary Casares <rrumble250@gmail.com>
2023-11-10 15:23:32 -0500

    add form input for composing tweet

--- client/src/components/feed/TweetComposition.jsx
+++ client/src/components/feed/TweetComposition.jsx
@@ -1,5 +1,24 @@
+import {useState} from "react";
 import Class from "./TweetComposition.module.css";
 
 export default function TweetComposition() {
-  return;
+  // TODO: Find out how to automatically stretch the
+  //       textarea on each line break while stretching
+  //       the container height.
+  const [rows, setRows] = useState(1);
+
+  return (
+    <div className={Class.container}>
+      <div className={Class.inputGroup}>
+        <div className={Class.img}>
+          <div></div>
+          <img src="placeholderIMG.jpg" alt="" />
+        </div>
+        <form>
+          <textarea type="text" placeholder="What is happening?!" onChange={textAreaRowsHandler} rows={rows} />
+        </form>
+      </div>
+      <div className={Class.controlGroup}></div>
+    </div>
+  );
 }
