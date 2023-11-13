0c461ef7c0ce9d40fccc9a0101730c658d64b61d
Zachary Casares <rrumble250@gmail.com>
2023-11-13 18:34:59 -0500

    add the control group icons and post button

--- client/src/components/feed/TweetComposition.jsx
+++ client/src/components/feed/TweetComposition.jsx
@@ -1,5 +1,11 @@
 import {useState} from "react";
 import Class from "./TweetComposition.module.css";
+import {HiOutlinePhotograph} from "react-icons/hi";
+import {HiGif} from "react-icons/hi2";
+import {BsEmojiSmile} from "react-icons/bs";
+import {GoTasklist} from "react-icons/go";
+import {AiOutlineSchedule} from "react-icons/ai";
+import {SiGooglemaps} from "react-icons/si";
 
 export default function TweetComposition() {
   // TODO: Find out how to automatically stretch the
@@ -18,7 +24,17 @@ export default function TweetComposition() {
           <textarea type="text" placeholder="What is happening?!" rows={rows} />
         </form>
       </div>
-      <div className={Class.controlGroup}></div>
+      <div className={Class.controlGroup}>
+        <div>
+          <HiOutlinePhotograph />
+          <HiGif />
+          <GoTasklist />
+          <BsEmojiSmile />
+          <AiOutlineSchedule />
+          <SiGooglemaps />
+        </div>
+        <button>Post</button>
+      </div>
     </div>
   );
 }
